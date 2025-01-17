243f7ddb53eefa7b6c5501e7141008b85e920503
jeran15 <jeranmiranda0306@gmail.com>
2025-01-16 23:21:48 -0800

    Add middleware for logging requests and implement product API endpoints

---
+++ 2-Express-Tutorial/express-basic.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.use(express.static('./public'));
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'));
+// adding to static assets
+// SSR - server side rendering
+// })
+
+app.get('/about',(req,res)=>{
+    res.send('About Page');
+})
+
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>Resource not found</h1>');
+})
+
+app.listen(5000,()=>{
+    console.log('Server is listening on port 5000');
+})
---
+++ 2-Express-Tutorial/rout-params.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const { products } = require('./data')
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+// Filter the products array to only include the id, name, and image keys.
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product;
+        return {id,name,image};
+    })
+    res.json(newProducts)
+})
+// Filter the product using the id.
+app.get('/api/products/:productID',(req,res)=>{
+    //console.log(req.params)
+    const {productID} = req.params;
+    const singleProduct = products.find((product)=> product.id === Number(productID) )
+
+    if(!singleProduct){
+        return res.status(404).send('Product does not exist')
+    }
+    res.json(singleProduct)
+})
+
+app.listen(5000,()=>{
+    console.log('Server is listening on port 5000...')
+})
